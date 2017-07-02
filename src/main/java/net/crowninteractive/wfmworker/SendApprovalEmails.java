1b064e2d9dc9cc2244f7f465937ff395a328836a
osita@push.ng <osita@push.ng>
2017-07-02 22:47:19 +0100

    fixed bug where one email was being  set per item in request

--- src/main/java/net/crowninteractive/wfmworker/SendApprovalEmails.java
+++ src/main/java/net/crowninteractive/wfmworker/SendApprovalEmails.java
@@ -17,8 +17,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -42,36 +45,76 @@ public class SendApprovalEmails extends TimerTask {
         try {
             List<InventoryApproval> inventoryApprovals = inventoryApprovalDao.findByEmailSendTimeNULL();
             System.out.println("SendApprovalEmails running...");
+            
+            if(inventoryApprovals != null && inventoryApprovals.size() > 0){
+                Map<Integer, List<InventoryApproval>> requests = getRequests(inventoryApprovals);
+                for (Integer workorderId  : requests.keySet()) {
+                    InventoryApproval inventoryApproval = requests.get(workorderId).get(0);
+                    
+                    WorkOrder workOrder = inventoryApproval.getWorkOrderId();
+                    String[] recipients = inventoryApproval.getRecipients().split(",");
+                    String template;
+                    template = EmailText.getApprovalContent();
 
-            for (InventoryApproval inventoryApproval : inventoryApprovals) {
-                InventoryRequest request = inventoryApproval.getInventoryReqId();
-                WorkOrder workOrderId = inventoryApproval.getWorkOrderId();
-                String[] recipients = inventoryApproval.getRecipients().split(",");
-                String template;
-                template = EmailText.getApprovalContent();
+                    System.out.println("SendApprovalEmails processing #"+ workOrder.getTicketId());
 
-                System.out.println("SendApprovalEmails processing "+ request);
-               
-                String url = Config.getInstance().wfmFrontendUrl();
-                for (String recipient : recipients) {
-                    String content = String.format(template,workOrderId.getSummary(), workOrderId.getDescription(), request.getItemName(), request.getLocationName(), request.getQuantity().toString(),url).replaceFirst("(%%)","%");
+                    String url = Config.getInstance().wfmFrontendUrl();
+                    String items = getItems(requests.get(workorderId));
+                    for (String recipient : recipients) {
+                        String content = String.format(template,workOrder.getSummary(), workOrder.getDescription(), items,url).replaceFirst("(%%)","%");
 
-                    System.out.println("SendApprovalEmails to " + recipient);
-                    emailSender.sendNoReplyEmail(
-                            "INVENTORY APPROVAL FOR WORK ORDER #" + workOrderId.getTicketId(), recipient,
-                            content);
-                    System.out.println("SendApprovalEmails sent to " + recipient);
+                        System.out.println("SendApprovalEmails to " + recipient);
+                        emailSender.sendNoReplyEmail(
+                                "INVENTORY APPROVAL FOR WORK ORDER #" + workOrder.getTicketId(), recipient,
+                                content);
+                        System.out.println("SendApprovalEmails sent to " + recipient);
+                    }
+                    inventoryApproval.setEmailSendTime(new Date());
+                    inventoryApproval.setUpdateTime(new Date());
+                    inventoryApprovalDao.edit(inventoryApproval);
+                    System.out.println("SendApprovalEmails done processing inventory " + inventoryApproval);
                 }
-                inventoryApproval.setEmailSendTime(new Date());
-                inventoryApproval.setUpdateTime(new Date());
-                inventoryApprovalDao.edit(inventoryApproval);
-                System.out.println("SendApprovalEmails done processing inventory " + inventoryApproval);
+            
+            
             }
             System.out.println("SendApprovalEmails run ended successfully!");
         } catch (IOException ex) {
             System.out.println("SendApprovalEmails an exception occurred  " + ex);
             Logger.getLogger(SendApprovalEmails.class.getName()).log(Level.ALL, null, ex);
         }
+    }
+
+    private Map<Integer, List<InventoryApproval>> getRequests(List<InventoryApproval> inventoryApprovals) {
+        Map<Integer, List<InventoryApproval>> m = new LinkedHashMap<>();
+        for(InventoryApproval iv : inventoryApprovals){
+            WorkOrder w = iv.getWorkOrderId();
+            if(w != null){
+                if(m.get(w.getId()) == null){
+                    List<InventoryApproval> l = new ArrayList();
+                    l.add(iv);
+                    m.put(w.getId(), l);
+                }else{
+                    List<InventoryApproval> l = m.get(w.getId());
+                    l.add(iv);
+                    m.put(w.getId(), l);
+                }
+            }
+        }
+        
+        return m;
+    }
+
+    private String getItems(List<InventoryApproval> get) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<tr>");
+        for(InventoryApproval inventoryApproval : get){
+            InventoryRequest request = inventoryApproval.getInventoryReqId();
+            sb.append("<td style='padding: 10px;'>").append(request.getItemName()).append("</td>");
+            sb.append("<td style='padding: 10px;'>").append(request.getLocationName()).append("</td>");
+            sb.append("<td style='padding: 10px;'>").append(request.getQuantity()).append("</td>");
+        }
+        sb.append("</tr>");
+        return sb.toString();
     }
 }
 
