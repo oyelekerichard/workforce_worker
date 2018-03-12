d20604481f718ed23f6d9f649228f95e89b2fc3b
johnsoneyo <johnsoneyo@gmail.com>
2018-03-12 16:46:10 +0100

    red

--- src/main/java/net/crowninteractive/wfmworker/cron/LoadStatusContextListener.java
+++ src/main/java/net/crowninteractive/wfmworker/cron/LoadStatusContextListener.java
@@ -5,13 +5,8 @@
  */
 package net.crowninteractive.wfmworker.cron;
 
-import java.util.Arrays;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import net.crowninteractive.wfmworker.ApplicationContextProvider;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
  *
@@ -22,9 +17,8 @@ public class LoadStatusContextListener implements ServletContextListener {
 
   
     @Override
-    public void contextInitialized(ServletContextEvent sce) {
-      ApplicationContext ac = ApplicationContextProvider.getApplicationContext();
-        System.out.println(">>>>> Bean definition >>>>>>>>>>>>>>>>>>>>>"+Arrays.toString(ac.getBeanDefinitionNames()));
+    public void contextInitialized(final ServletContextEvent sce) {
+        System.out.println(">>>>>>>>>>>>>>>>Sce >>>>>>>>>>>>>>>>>>>>>"+sce);
     }
 
     @Override
