148b96506a769aa228e46ab6f3c3de10c11f23c7
johnsoneyo <johnsoneyo@gmail.com>
2018-03-13 10:49:13 +0100

    rollback

--- src/main/java/net/crowninteractive/wfmworker/cron/LoadStatusContextListener.java
+++ src/main/java/net/crowninteractive/wfmworker/cron/LoadStatusContextListener.java
@@ -27,7 +27,7 @@ public class LoadStatusContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(sce.getServletContext());
-        dao = ctx.getBean(WorkOrderDao.class);
+       // dao = ctx.getBean(WorkOrderDao.class);
       
     }
 
