f44cdfd7a0c705a68678ea1898887b2d35029104
johnsoneyo <johnsoneyo@gmail.com>
2018-03-13 18:05:39 +0100

    ready

--- src/main/java/net/crowninteractive/wfmworker/cron/WorkOrderObservable.java
+++ src/main/java/net/crowninteractive/wfmworker/cron/WorkOrderObservable.java
@@ -34,7 +34,7 @@ public class WorkOrderObservable extends Observable implements Runnable{
     public void run() {
         try {
             ApplicationContext ac = new ClassPathXmlApplicationContext(
-                    "classpath*:spring-config.xml");
+                    "WEB-INF/spring-config.xml");
             System.out.println(">>>>>>BEan Def NAme >>>>>>>>>>>>>>>>>>>>>>>>>"+Arrays.toString(ac.getBeanDefinitionNames()));
             dao = ac.getBean(WorkOrderDao.class);
          
