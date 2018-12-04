aa76e080c5b9eb43da845dac3a5f1cb9fbe53cc5
johnsoneyo <johnsoneyo@gmail.com>
2018-12-04 09:46:28 +0100

    is active

--- src/test/java/net/crowninteractive/wfmworker/MikaSpecialReport.java
+++ src/test/java/net/crowninteractive/wfmworker/MikaSpecialReport.java
@@ -14,9 +14,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import net.crowninteractive.wfmworker.dao.AbstractDao;
 import net.crowninteractive.wfmworker.dao.UsersDao;
 import net.crowninteractive.wfmworker.dao.WorkOrderDao;
+import net.crowninteractive.wfmworker.entity.Users;
 import net.crowninteractive.wfmworker.entity.WorkOrder;
 import net.crowninteractive.wfmworker.exception.WfmWorkerException;
 import net.crowninteractive.wfmworker.service.SendEmail;
@@ -25,12 +25,10 @@ import org.apache.commons.mail.EmailException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Font;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -68,16 +66,25 @@ public class MikaSpecialReport {
         runnable.run();
     }
 
-    public void processWriteV2(String from, String to, String email) throws FileNotFoundException, IOException, EmailException, WfmWorkerException {
-
-        String commaSeparated = usersDao.findByEmail(email).getDistricts();
+   public void processWriteV2(String from, String to, String email) throws FileNotFoundException, IOException, EmailException, WfmWorkerException {
+        System.out.println("-------------------------About to process report ---------");
+        System.out.println("userDao-----------" + this.usersDao);
+        System.out.println("-Email-------------" + email);
+        Users u = usersDao.findByEmail(email);
+        if (u == null) {
+            System.out.println(">>>>>>>>No district for user >>>>>>>>.." + email);
+            return;
+        }
+        String commaSeparated = u.getDistricts();
+        System.out.println("-----Districts are ------ " + commaSeparated);
 
         String queueTypeId = usersDao.getQueueTypeIds(email);
         String tariffs = usersDao.getAssignedTariffs(email);
 
         List<String> districts = new ArrayList<String>(Arrays.asList(commaSeparated.split("\\s*,\\s*")));
+        System.out.println("districts >>>>>>" + districts);
 
-        Workbook workbook = new XSSFWorkbook();
+        SXSSFWorkbook workbook = new SXSSFWorkbook();
 
         CellStyle headerStyle = workbook.createCellStyle();
         Font font = workbook.createFont();
@@ -85,19 +92,23 @@ public class MikaSpecialReport {
 
         CellStyle h = workbook.createCellStyle();
         Font f = workbook.createFont();
-        //f.setBold(true);
+        f.setBoldweight((short) 4);
         f.setFontHeight((short) 300);
         h.setFont(f);
-        //h.setAlignment(HorizontalAlignment.CENTER);
+        h.setAlignment((short) 5);
 
         //font.setBold(true);
         headerStyle.setFont(font);
 
         for (int i = 0; i < districts.size(); i++) {
 
-            List<WorkOrder> lwListt = wdao.getWorkOrderByParamsV2(districts.get(i), from, to, queueTypeId, tariffs);
+            long st = System.currentTimeMillis();
+            List<WorkOrder> lwListt = wdao.getWorkOrderByParams(districts.get(i), from, to, queueTypeId, tariffs);
+            long en = System.currentTimeMillis();
+            long timeTaken = en - st;
 
-            Sheet sheet = workbook.createSheet(districts.get(i));
+            SXSSFSheet sheet = (SXSSFSheet) workbook.createSheet(districts.get(i));
+            sheet.setRandomAccessWindowSize(100);
 
             Cell info0 = sheet.createRow(2).createCell(0);
             info0.setCellStyle(h);
@@ -133,6 +144,7 @@ public class MikaSpecialReport {
 
             int rownum = 6;
             double count = 1;
+
             for (WorkOrder w : lwListt) {
                 Row row = sheet.createRow(rownum++);
                 Cell cell = row.createCell(0);
@@ -143,7 +155,7 @@ public class MikaSpecialReport {
                 try {
                     cName = wdao.getCustomerName(w);
                 } catch (Exception e) {
-
+                    e.printStackTrace();
                 }
                 row.createCell(2).setCellValue(cName);
                 row.createCell(3).setCellValue(w.getAddressLine1() + ", " + w.getCity());
