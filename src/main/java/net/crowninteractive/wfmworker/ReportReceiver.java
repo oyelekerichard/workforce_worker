08428bbd08aed6e798e66b66a17f2eb722394c05
johnsoneyo <johnsoneyo@gmail.com>
2018-12-04 13:25:42 +0100

    priority

--- src/main/java/net/crowninteractive/wfmworker/ReportReceiver.java
+++ src/main/java/net/crowninteractive/wfmworker/ReportReceiver.java
@@ -6,8 +6,6 @@
 package net.crowninteractive.wfmworker;
 
 import com.dyfferential.vyral.web.util.DateFormatter;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -32,8 +30,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
@@ -67,67 +64,6 @@ public class ReportReceiver {
             }
         }
 
-    }
-
-    @Deprecated
-    private void processWrite(String from, String to, String email) throws FileNotFoundException, IOException, EmailException {
-        FileInputStream file = new FileInputStream(new File("/var/files/wfm/nerctemplate.xlsx"));
-
-        //String[] mths = {"-", "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
-        String[] districts = {"APAPA", "MUSHIN", "AGBARA", "ISLANDS", "IJORA", "LEKKI", "OJO", "FESTAC", "UNKNOWN", "IBEJU", "ORILE"};
-
-        XSSFWorkbook workbook = new XSSFWorkbook(file);
-
-        for (int i = 0; i < districts.length; i++) {
-
-            List<WorkOrder> lwListt = wdao.getWorkOrderByParams(districts[i], from, to);
-
-            XSSFSheet sheet = workbook.getSheetAt(i);
-
-            sheet.getRow(3).getCell(13).setCellValue(DateFormatter.getSimpleDate(from) + " - " + DateFormatter.getSimpleDate(to));
-            int rownum = 6;
-            double count = 1;
-            for (WorkOrder w : lwListt) {
-                Row row = sheet.createRow(rownum++);
-                Cell cell = row.createCell(0);
-                if (cell.getCellType() == Cell.CELL_TYPE_BLANK) {
-                    cell.setCellType(Cell.CELL_TYPE_STRING);
-                    cell.setCellValue(count++);
-                } else {
-                    cell.setCellValue(count++);
-                }
-
-                row.createCell(1).setCellValue(w.getTicketId());
-                row.createCell(2).setCellValue(w.getCustomerName()==null ? "":w.getCustomerName());
-                row.createCell(3).setCellValue(w.getAddressLine1() + ", " + w.getCity());
-                row.createCell(4).setCellValue(getBillingId(w));
-                row.createCell(5).setCellValue(String.valueOf(w.getContactNumber()));
-                row.createCell(6).setCellValue(String.valueOf(w.getCustomerTariff()));
-                row.createCell(7).setCellValue(String.valueOf(w.getBusinessUnit()));
-                row.createCell(8).setCellValue(String.valueOf(w.getSummary()));
-                row.createCell(9).setCellValue(String.valueOf(w.getCreateTime()));
-                String status = "";
-                if (w.getIsClosed() != null) {
-                    if (w.getCurrentStatus() != null) {
-                        status = w.getIsClosed() == 0 ? w.getCurrentStatus() : "CLOSED";
-                    }
-                }
-                row.createCell(10).setCellValue(status);
-                row.createCell(11).setCellValue(wdao.getDateResolved(w));
-                row.createCell(12).setCellValue(getResolution(w));
-                row.createCell(13).setCellValue(String.valueOf(wdao.getQueueTypeName(1, w.getQueueTypeId())));
-
-            }
-        }
-
-        String localfile = "generated_" + RandomStringUtils.randomAlphanumeric(5) + "_" + System.currentTimeMillis() + ".xlsx";
-        String report = "/var/files/email/" + localfile;
-        FileOutputStream outputStream = new FileOutputStream(report);
-        workbook.write(outputStream);
-        System.out.println("file is " + report);
-
-        emm.sendAnEmail("a12wq_minions", "NERC customer care report", email, "Your report is now ready. Please find attached the requested report", localfile, null);
-        System.out.println("report processor - Message Sent");
     }
 
     private String getBillingId(WorkOrder w) {
@@ -168,28 +104,24 @@ public class ReportReceiver {
         List<String> districts = new ArrayList<String>(Arrays.asList(commaSeparated.split("\\s*,\\s*")));
         System.out.println("districts >>>>>>" + districts);
 
-        SXSSFWorkbook workbook = new SXSSFWorkbook();
-
+        SXSSFWorkbook workbook = new SXSSFWorkbook(100);
+       
         CellStyle headerStyle = workbook.createCellStyle();
         Font font = workbook.createFont();
         font.setFontHeight((short) 200);
 
         CellStyle h = workbook.createCellStyle();
         Font f = workbook.createFont();
-        f.setBoldweight((short) 4);
+        f.setBoldweight(XSSFFont.BOLDWEIGHT_BOLD);
         f.setFontHeight((short) 300);
         h.setFont(f);
-        h.setAlignment((short) 5);
+        h.setAlignment(CellStyle.ALIGN_CENTER);
 
-        //font.setBold(true);
         headerStyle.setFont(font);
 
         for (int i = 0; i < districts.size(); i++) {
 
-            long st = System.currentTimeMillis();
             List<WorkOrder> lwListt = wdao.getWorkOrderByParams(districts.get(i), from, to, queueTypeId, tariffs);
-            long en = System.currentTimeMillis();
-            long timeTaken = en - st;
 
             SXSSFSheet sheet = (SXSSFSheet) workbook.createSheet(districts.get(i));
             sheet.setRandomAccessWindowSize(100);
@@ -219,7 +151,7 @@ public class ReportReceiver {
                 "Phone No", "Tariff Class", "B/Unit",
                 "Nature of Complaint", "Date Received",
                 "Action Taken", "Date Resolved", "Resolution", "Category"};
-            for (int col = 0; col < 13; col++) {
+            for (int col = 0; col < 14; col++) {
                 Cell cell = xheader.createCell(col);
                 cell.setCellValue(header[col]);
                 cell.setCellStyle(headerStyle);
@@ -236,8 +168,13 @@ public class ReportReceiver {
 
                 row.createCell(1).setCellValue(w.getTicketId());
                 String cName = "";
-               
-                row.createCell(2).setCellValue(w.getCustomerName()==null ? "":w.getCustomerName());
+                int c = Cell.CELL_TYPE_BLANK;
+                if (w.getCustomerName() == null) {
+                    row.createCell(2).setCellValue(Cell.CELL_TYPE_BLANK);
+                } else {
+                    row.createCell(2).setCellValue(w.getCustomerName());
+                }
+
                 row.createCell(3).setCellValue(w.getAddressLine1() + ", " + w.getCity());
                 row.createCell(4).setCellValue(getBillingId(w));
                 row.createCell(5).setCellValue(String.valueOf(w.getContactNumber()));
@@ -251,7 +188,6 @@ public class ReportReceiver {
                         status = w.getIsClosed() == 0 ? w.getCurrentStatus() : "CLOSED";
                     }
                 }
-
                 row.createCell(10).setCellValue(status);
                 row.createCell(11).setCellValue(wdao.getDateResolved(w));
                 row.createCell(12).setCellValue(getResolution(w));
@@ -264,6 +200,8 @@ public class ReportReceiver {
         String report = "/var/files/email/" + localfile;
         FileOutputStream outputStream = new FileOutputStream(report);
         workbook.write(outputStream);
+        outputStream.close();
+       
         System.out.println("file is " + report);
 
         emm.sendAnEmail("a12wq_minions", "NERC customer care report", email, "Your report is now ready. Please find attached the requested report", localfile, null);
