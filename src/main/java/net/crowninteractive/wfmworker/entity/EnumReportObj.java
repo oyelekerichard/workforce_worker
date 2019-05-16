a6fd1fbaa1b02065bfd53fe31c315eeb2f95f514
Isaac adegunle <isaac.adegunle@gmail.com>
2019-05-16 09:40:39 +0100

    added download enumeration report endpoit

---
+++ src/main/java/net/crowninteractive/wfmworker/entity/EnumReportObj.java
@@ -0,0 +1,68 @@
+package net.crowninteractive.wfmworker.entity;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Isaac A.
+ */
+public class EnumReportObj {
+    private String type;
+    private String[]tokens;
+    private String email;
+    private String district;
+    private String fromDate;
+    private String toDate;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String[] getTokens() {
+        return tokens;
+    }
+
+    public void setTokens(String[] tokens) {
+        this.tokens = tokens;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(String fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
+}
